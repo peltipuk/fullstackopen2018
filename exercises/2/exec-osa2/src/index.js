23a8c6e3e1236a28cd5bc01a42acae3561107676
Lauri Vanhatalo <lauri.vanhatalo@nokia.com>
2018-11-21 13:55:57 +0200

    Exercise 2.5

--- exercises/2/exec-osa2/src/index.js
+++ exercises/2/exec-osa2/src/index.js
@@ -1,37 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Otsikko = (props) => (
-  <h1>{props.kurssi}</h1>
-)
-
-const Osa = (props) => (
-  <p>{props.nimi} {props.tehtavia}</p>
-)
-
-const Sisalto = (props) => (
-  <div>
-    {
-      props.osat.map(osa => (
-        <Osa nimi={osa.nimi} tehtavia={osa.tehtavia} />
-      ))
-    }
-  </div>
-)
-
-const Yhteensa = (props) => {
-  return <p>yhteensä {props.osat.reduce((sum, cur) => sum + cur.tehtavia, 0)} tehtävää</p>
-}
-
-const Kurssi = (props) => {
-  return (
-    <div>
-      <Otsikko kurssi={props.kurssi.nimi} />
-      <Sisalto osat={props.kurssi.osat} />
-      <Yhteensa osat={props.kurssi.osat} />
-    </div>
-  )
-}
+import Kurssi from './components/Kurssi'
 
 const App = () => {
   const kurssit = [
