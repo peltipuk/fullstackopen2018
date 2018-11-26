fff9e02d8282669416e6c9fd789475e08325bafe
Lauri Vanhatalo <lauri.vanhatalo@nokia.com>
2018-11-26 10:57:47 +0200

    Adds deploy script and specifies proxy

---
+++ material/2/osa2/build/precache-manifest.2a77f0c89d5c3b16908b88e1eb9b3dbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5481e6410e0b14944c4a",
+    "url": "/static/js/main.5481e641.chunk.js"
+  },
+  {
+    "revision": "097107a0fc6c17120c1f",
+    "url": "/static/js/1.097107a0.chunk.js"
+  },
+  {
+    "revision": "5481e6410e0b14944c4a",
+    "url": "/static/css/main.c4f2c6b4.chunk.css"
+  },
+  {
+    "revision": "6fc51da5702bc4106d940cc77edb4633",
+    "url": "/index.html"
+  }
+];
---
+++ material/2/osa2/deploy.sh
@@ -0,0 +1,3 @@
+npm run build
+rm -rf ~/sandbox/fullstackopen2018-notes-backend/build
+cp -r build ~/sandbox/fullstackopen2018-notes-backend
